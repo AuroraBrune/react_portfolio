6a1fa1ef4faede755e0cf18fd8816c598a305c9e
Aurora Brune <akbrune@hotmail.com>
2020-11-01 16:57:57 -0600

    moved nav component to header

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -2,9 +2,9 @@ import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 // import "bootstrap/dist/css/bootstrap.min.css;"
 import Header from "./components/Header/header";
-import Navbar from "./components/Navbar/navbar";
+
 import Wrapper from "./components/Wrapper/wrapper";
-import NavTabs from "./components/NavTabs/navtabs";
+
 import Footer from "./components/Footer/footer";
 import Home from "./pages/Home";
 import Portfolio from "./pages/Portfolio";
@@ -14,9 +14,8 @@ function App() {
   return (
     <Router>
       <div>
-        <NavTabs />
+
         <Header />
-        <Navbar />
         <Wrapper>
           <Route exact path="/" component={Home} />
           <Route exact path="/portfolio" component={Portfolio} />
